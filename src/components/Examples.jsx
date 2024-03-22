bec95b570ccc6127112a0714214f80fa77801161
Kirushanthi26 <kirushanthikiru26@gmail.com>
2024-03-22 11:19:13 +0530

    2 child component common fn on main component

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import TabButton from "./TabButton";
 import Section from "./Section";
+import { EXAMPLES } from "../data";
 
 const Examples = () => {
+  const handleSelect = () => {};
+
   return (
     <Section id="examples" title={"Examples"}>
       <menu>
-        <TabButton>Components</TabButton>
+        <TabButton onSelect={handleSelect}>Components</TabButton>
+        <TabButton onSelect={handleSelect}>JSX</TabButton>
+        <TabButton onSelect={handleSelect}>Props</TabButton>
+        <TabButton onSelect={handleSelect}>State</TabButton>
       </menu>
+      btn click, dynamic content should change, so, onClick create here
     </Section>
   );
 };
